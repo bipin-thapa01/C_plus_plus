dadf050eeca44aa89478918f4b279fa2b2a2ebd6
Bipin Thapa <yukichin638@gmail.com>
2024-06-27 21:04:57 +0545

    minor code

---
+++ default_argument.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+float potentialEnergy(float m, float h, float g=9.8)
+{
+	return m*g*h;
+}
+
+float areaOfCricle(float r, float pi = 22.0/7)
+{
+	return pi*r*r;
+}
+
+float dollarsToRupee(float d, float rate=130)
+{
+	return d*rate;
+}
+
+int main()
+{
+	int choice;
+	float m,h,r,d;
+	top:
+	cout << "\t\tMenu" << endl;
+	cout << "1. potential energy" << endl;
+	cout << "2. area of circle" << endl;
+	cout << "3. dollar to nepali" << endl;
+	cout << "Enter your choice: ";
+	cin >> choice;
+	if(choice == 1)
+	{
+		cout << "Enter mass and height: ";
+		cin >> m >> h;
+		cout << "Potential Engery is: " << potentialEnergy(m,h) <<" joule";
+	}
+	else if(choice == 2)
+	{
+		cout << "Enter radius of circle: ";
+		cin >> r;
+		cout << "Area of circle is: " << areaOfCricle(r) <<" sq. meter";
+	}
+	else if(choice == 3)
+	{
+		cout << "Enter money in rupees: ";
+		cin >> d;
+		cout << "$ " << d << " is Rs " << dollarsToRupee(d);
+	}
+	else
+	{
+		cout << "Invalid input! Try again." << endl << endl;
+		goto top;
+	}
+	return 0;
+}
