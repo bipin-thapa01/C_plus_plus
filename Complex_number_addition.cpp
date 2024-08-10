bbc6f2ec5900f2419f27c3a3c8eeff9760e814fe
Bipin Thapa <yukichin638@gmail.com>
2024-08-10 19:21:38 +0545

    Binary opreator overloading

---
+++ Complex_number_addition.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+	private:
+		float r,i;
+	public:
+		void input()
+		{
+			cout << "enter real part: ";
+			cin >> r;
+			cout << "enter imaginary part: ";
+			cin >> i;
+			cout << endl;
+		}
+		
+		Complex operator +(const Complex& obj)
+		{
+			Complex temp;
+			temp.r = r + obj.r;
+			temp.i = i + obj.i;
+			return temp;
+		}
+		
+		void output()
+		{
+			cout << "Sum: " << r << "+ " << i <<"i";
+		}
+};
+
+int main()
+{
+	Complex num1,num2,sum;
+	num1.input();
+	num2.input();
+	sum = num1 + num2;
+	sum.output();
+	return 0;
+}
